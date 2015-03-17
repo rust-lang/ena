24064afae57495b6b1d9c08f12c51ed95113a42b
Niko Matsakis <niko@alum.mit.edu>
2015-03-17 05:37:49 -0400

    Add some more tests for unification engine

--- src/test.rs
+++ src/test.rs
@@ -20,3 +20,47 @@ fn basic() {
     assert_eq!(ut.unioned(k1, k2), true);
 }
 
+#[test]
+fn big_array() {
+    let mut ut: UnificationTable<UnitKey> = UnificationTable::new();
+    let mut keys = Vec::new();
+    const max: usize = 1 << 20;
+
+    for _ in 0..max {
+        keys.push(ut.new_key(()));
+    }
+
+    for i in 1..max {
+        let l = keys[i-1];
+        let r = keys[i];
+        ut.union(l, r);
+    }
+
+    for i in 0..max {
+        assert!(ut.unioned(keys[0], keys[i]));
+    }
+}
+
+#[test]
+fn even_odd() {
+    let mut ut: UnificationTable<UnitKey> = UnificationTable::new();
+    let mut keys = Vec::new();
+    const max: usize = 1 << 10;
+
+    for i in 0..max {
+        let key = ut.new_key(());
+        keys.push(key);
+
+        if i >= 2 {
+            ut.union(key, keys[i-2]);
+        }
+    }
+
+    for i in 1..max {
+        assert!(!ut.unioned(keys[i-1], keys[i]));
+    }
+
+    for i in 2..max {
+        assert!(ut.unioned(keys[i-2], keys[i]));
+    }
+}
